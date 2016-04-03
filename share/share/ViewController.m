4160382643ff770251e85c2fbe2fedaa2a68360a
Matthew McClure <matthewmcclure@Matthews-MacBook-Pro.local>
2016-04-03 14:34:25 -0500

    sharing text or image works

--- share/share/ViewController.m
+++ share/share/ViewController.m
@@ -3,12 +3,18 @@
 //  share
 //
 //  Created by Matthew McClure on 4/3/16.
-//  Copyright © 2016 Matthew McClure. All rights reserved.
+/*
+ thanks to:
+ http://www.codingexplorer.com/add-sharing-to-your-app-via-uiactivityviewcontroller/
+ */
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
+
+- (IBAction)share:(UIButton *)sender;
+
 
 @end
 
@@ -24,4 +30,29 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)share:(UIButton *)sender {
+    
+    UIImage *image = [UIImage imageNamed:@"alice"];
+    
+    NSString *textToShare = @"Look at this awesome website for aspiring iOS Developers!";
+    
+    NSURL *myWebsite = [NSURL URLWithString:@"http://www.codingexplorer.com/"];
+    
+    NSArray *objectsToShare = @[textToShare, myWebsite, image];
+    
+    UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:objectsToShare applicationActivities:nil];
+    
+    NSArray *excludeActivities = @[UIActivityTypeAirDrop,
+                                   UIActivityTypePrint,
+                                   UIActivityTypeAssignToContact,
+                                   UIActivityTypeSaveToCameraRoll,
+                                   UIActivityTypeAddToReadingList,
+                                   UIActivityTypePostToFlickr,
+                                   UIActivityTypePostToVimeo];
+    
+    activityVC.excludedActivityTypes = excludeActivities;
+    
+    [self presentViewController:activityVC animated:YES completion:nil];
+    
+}
 @end
