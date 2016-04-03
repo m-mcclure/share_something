db8a4464970895f1df2c21ebd2d8ef9fe49674d8
Matthew McClure <matthewmcclure@Matthews-MacBook-Pro.local>
2016-04-03 14:05:25 -0500

    add proj

---
+++ share/share/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  share
+//
+//  Created by Matthew McClure on 4/3/16.
+//  Copyright © 2016 Matthew McClure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ share/share/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  share
+//
+//  Created by Matthew McClure on 4/3/16.
+//  Copyright © 2016 Matthew McClure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
